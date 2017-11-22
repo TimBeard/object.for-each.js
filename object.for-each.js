4505e09502bc8ca69b856a656b95d1acae970b67
Timothy Beard <7124813+TimBeard@users.noreply.github.com>
2017-11-22 17:08:24 +0100

    Create object.for-each.js

---
+++ object.for-each.js
@@ -0,0 +1,19 @@
+(function (proto) {
+
+    "use strict";
+
+    var forEach = function (cb) {
+
+        var obj = this;
+
+        return Object.getOwnPropertyNames(this).forEach(function (key) {
+
+            return cb(obj[key], key, obj);
+        });
+    };
+
+    if (Object.prototype.forEach === undefined) {
+
+        Object.defineProperty(proto, "forEach", {value: forEach});
+    }
+}(Object.prototype));
